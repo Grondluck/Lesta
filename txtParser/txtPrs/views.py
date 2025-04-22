d4dcb08ae4ae1f6b3b2f79409c6d65ae5518448d
Grondluck <grondluck@gmail.com>
2025-04-22 17:44:54 +0300

    Проделать тесты, похоже всё ОК. Сделать сортировку по IDF. Сделать ограничение на кол-во строк в таблице до 50. Мб чуть поровнять столбцы

--- txtParser/txtPrs/views.py
+++ txtParser/txtPrs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
 import re
-# Create your views here.
+import math
+from django.shortcuts import render, redirect
+
 
 def index(request):
     tables = request.session.get('tables', [])
@@ -12,16 +13,36 @@ def index(request):
             text = re.sub(r'[^\w\s]', '', text)
             text = text.replace('\r', ' ').replace('\n', ' ').split(" ")
             words = list(filter(None, text))
-            wordsLst = []
+            words = list(map(str.lower, words))
             wordsDict = {}
             for word in words:
-                if word not in wordsLst:
-                    wordsLst.append(word)
-                    wordsDict[word] = {"tf":1, "idf":0}
+                if word not in wordsDict.keys():
+                    wordsDict[word] = {"pretf":1, "tf":0, "preidf":0, "idf":0}
                 else:
-                    wordsDict[word]["tf"] += 1
-            tables.append({wordsLst:wordsDict})
+                    wordsDict[word]["pretf"] += 1                    
+                    
+            tables.append(wordsDict)
+
+
+            for table in tables:
+                for word in table:
+                    table[word]["preidf"] = 0
+                    for i in range(len(tables)):
+                        if word in tables[i].keys():
+                            table[word]["preidf"] += 1
+
+                                
+                    table[word]["idf"] = math.log10( len(tables) / table[word]["preidf"])
+                    print(word, "preidf", table[word]["preidf"], len(tables))
+                    table[word]["tf"] = table[word]["pretf"] / len(table.keys())
+                    #print(f"tf: {word}", table[word]["pretf"], len(table.keys()), table[word]["tf"])
             request.session['tables'] = tables
             return redirect('index')
+        if 'reset' in request.POST:
+            request.session['tables'] = []
+            request.session.modified = True
+            return redirect('index')
+    
+        
 
     return render(request, "index.html", {"tables":tables})
